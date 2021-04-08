cc398590c34968379f14422da9ff86fdf1eecd8d
Changsin Park <ics19-006@camosun.bc.ca>
2021-04-08 17:07:17 +0000

    lab8 part2 finished

--- components/MessageBoard.js
+++ components/MessageBoard.js
@@ -1,18 +1,21 @@
+import jwt_decode from 'jwt-decode';
 import { Container, Row, Col } from 'react-bootstrap';
+import { useState, useRef } from 'react';
 import Title from './Title';
 import Tabled from './Tabled';
 import NewMessageForm from './NewMessageForm';
 import LoginForm from './LoginForm';
-import { useState } from 'react';
 import ky from 'ky-universal';
 
 const MessageBoard = ({jsonData}) => {
+  const usernameRef = useRef(null);
   const [data, setData] = useState([...jsonData]);
   const [user, setUser] = useState(false)
 // handler for submission of Form in
 // NewMessageForm Component
 const addNewMessage = async (values) => {
     try{
+      values.name = usernameRef.current
       const message = await ky.post(`${process.env.NEXT_PUBLIC_HOST}/api/messages`, {
         json: values,
           headers: {
@@ -29,10 +32,12 @@ const addNewMessage = async (values) => {
 
 const logInUser = async (values) => {
   try{
-    const logIn = await ky.post(`${process.env.NEXT_PUBLIC_HOST}/api/login`, {
+    const response = await ky.post(`${process.env.NEXT_PUBLIC_HOST}/api/login`, {
       json: values
     }).json();
-    sessionStorage.setItem('token', logIn.token);
+    sessionStorage.setItem('token', response.token);
+    const decodedTokenPayload = jwt_decode(response.token);
+    usernameRef.current = values.username;
     setUser(true)
     } catch(err) {
       console.log('API error: ' + err);
@@ -47,7 +52,6 @@ const logInUser = async (values) => {
         </Row >
         <Row>
           <Col>{ user == true ? (<NewMessageForm addNewMessage={addNewMessage}/>) : (<LoginForm logInUser={logInUser}/>)}</Col>
-          {console.log(data)}
         </Row>
         <Row>
             <Col>
